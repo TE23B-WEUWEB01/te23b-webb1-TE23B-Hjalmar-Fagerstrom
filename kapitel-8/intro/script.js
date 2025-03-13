0105a9225f457b1b02fdee4eaa94a6baa4085524
Hjalmar Fagerström <hjalmar.fagerstrom@elev.ga.ntig.se>
2025-03-13 11:07:00 +0100

    Tränat på js, variabler och konkatenering

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,11 @@
+// var tal1 = 10;
+// var tal2 = 59;
+// var summa = tal1 + tal2;
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+var fornamn = "Hjalmar";
+var efternamn = "Fagerström";
+var hobby = "spela";
+var kostnad = ".\nMin hobby kostar mellan 0 till 500 kr/år"
+var mening = fornamn + " " + efternamn + "'s hobby är att " + hobby + "" + kostnad;
+alert(mening);
