e701860b090f64908736edced311f5ada01692b2
Hjalmar Fagerström <hjalmar.fagerstrom@elev.ga.ntig.se>
2025-03-20 11:12:23 +0100

    Tränat på villkor

--- kapitel-8/grunder/script.js
+++ kapitel-8/grunder/script.js
@@ -36,8 +36,55 @@
 // läs in skat, 30%
 // skriv ut: din inkomst efter skatt är xxx med yy% i skatt
 
-var brutto = prompt("vad är din inkomst för skatt")
-var skatt = prompt("vilke prosent betallar du i skatt")
-var inkomst = Number(brutto) * (100 - Number(skatt)) / 100;
+// var brutto = prompt("vad är din inkomst för skatt")
+// var skatt = prompt("vilke prosent betallar du i skatt")
+// var inkomst = Number(brutto) * (100 - Number(skatt)) / 100;
 
-document.writeln("<h1>din inkomst efter skatt är " + inkomst + " med " + skatt + "% i skatt  </h1>");
+// document.writeln("<h1>din inkomst efter skatt är " + inkomst + " med " + skatt + "% i skatt  </h1>");
+
+// uppgift 7.1
+// let ålder = prompt("skriv din ålder");
+
+// if (ålder >= 18){
+//     console.log("du är vuxen.");
+//     document.write("du är vuxen.");
+// } else {
+   
+//     document.write("du är minderårig.");
+// }
+
+// uppgift 7.2
+// var grader = prompt("hur många grader är det ute?");
+
+// if (grader > 20) {
+//     document.write("det är varmt ute");
+// } else {
+//     document.write("det är kallt ute");
+// }
+
+// uppgift 7.3
+// var djur = prompt("vad är ditt favorit djur")
+
+// if (djur == "hund") {
+//     document.write("jag gillar också hundar");
+// } else {
+//     document.write("jag tycker inte om " + djur);
+// }
+
+// uppgift 7.4
+// var pengar = prompt("hur mycket pengar kommer du handla för")
+
+// if (pengar > 300) {
+//     document.write("du får rabatt!")
+// } else {
+//     document.write("du får ingen rabatt")
+// }
+
+// Uppgift 7.5
+// var lössen = prompt("skriv in lösenordet")
+
+// if (lössen == "duHarFel") {
+//     document.write("välkommen")
+// } else {
+//     document.write("fel lösenord")
+// }
