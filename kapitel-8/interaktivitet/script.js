abde6776ebd95d15549b77b5ef1d3819796a614f
Hjalmar Fagerström <hjalmar.fagerstrom@elev.ga.ntig.se>
2025-03-27 11:08:34 +0100

    Tränat på onclick, funktioner, querySelector()

---
+++ kapitel-8/interaktivitet/script.js
@@ -0,0 +1,12 @@
+function DarkPage() {  
+    document.body.style = "background: #000; color: #FFF;";
+}
+function LightPage() {  
+    document.body.style = "background: #FFF; color: #000;";
+}
+function KonstigRubrik() {  
+    document.querySelector("h1").style = "font-size: 85px; text-transform: uppercase; text-decoration: underline; font-family: 'Times New Roman', Times, serif; color: blueviolet; background-color: aqua;"
+}
+function TextVarning() {  
+    document.querySelector("p").style = "visibility: visible;";
+}
